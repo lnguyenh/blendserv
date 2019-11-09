44c9a52294aa1cbfd140c006ce203539b9186538
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2019-11-09 19:25:41 +0100

    Adapt blendserv to current usage

--- server/blendserv.py
+++ server/blendserv.py
@@ -1,137 +1,166 @@
-#!/usr/bin/env python3
-# Usage example:
-# Get current blender state:
-# curl --user user:pass http://hostname:port
-# Set current blender state (to on)
-# curl --user user:pass --data 'blender=1' http://hostname:port
-
-import http.server
-import socketserver
-import base64
-import urllib
+from gpiozero import LED
 import threading
-import RPIO
+import http.server
+import base64
+import json
 
-srv_info = {'port': 8192, # Listening port
-	    'host': '',   # Listening host
-	    'user': b'foop',
-	    'pass': b'froopberry'}
+USER = 'froop'
+PASSWORD = 'froopberry'
+TIMEOUT = 4
+PORT = 8192
 
-TIMEOUT_BLENDER_OFF = 3 * 60
-PIN_BLENDER = 18 # GPIO 1 on Raspberry Pi model B
+ON = 'on'
+OFF = 'off'
 
-class Blender():
 
-	pin = None
-	state = None
-	timeout = None
-	timeout_thread = None
-	
-	def __init__(self, pin, timeout):
-		self.pin = pin
+class Blender(object):
+
+	def __init__(self, timeout):
+		# https://gpiozero.readthedocs.io/en/stable/recipes.html#pin-numbering
+		# Connect the GPIO-18 pin and a ground pin (Ground 6 for example)
+		self.pin = LED(18)
 		self.timeout = timeout
-		RPIO.setup(self.pin, RPIO.OUT)
-		RPIO.setmode(RPIO.BCM)
-		self.set(False)
+		self.timeout_thread = None
+		self.state = 0
+		self.off()
 
 	def handle_timeout(self):
 		print("Timeout")
-		self.set(False)
+		self.off()
 
-	def set(self, state):
-		print('Setting GPIO pin {:} to {:}'.format(self.pin, state))
+	def ensure_blender_turns_off(self):
+		self.timeout_thread = threading.Timer(
+			self.timeout,
+			self.handle_timeout,
+			[]
+		)
+		self.timeout_thread.start()
 
-		if self.timeout_thread:
-			self.timeout_thread.cancel()
+	def on(self):
+		print('Setting blender to ON')
+		self.pin.on()
+		self.ensure_blender_turns_off()
 
-		RPIO.output(self.pin, state)
-		if state:
-			self.timeout_thread = threading.Timer(self.timeout, 
-							      self.handle_timeout, 
-							      [])
-			self.timeout_thread.start()
+		self.state = ON
 
-		if state == self.state:
-			print("State unchanged ({:})".format(state))
-			return
+	def off(self):
+		print('Setting blender to OFF')
+		self.pin.off()
+		self.state = OFF
 
-		self.state = state
+	def toggle(self):
+		if self.state == ON:
+			self.off()
+		else:
+			self.on()
 
-	def get(self):
-		return self.state
-#		return RPIO.input(self.pin)
+	def cleanup(self):
+		self.pin.close()
 
 
-class WebServer(http.server.SimpleHTTPRequestHandler):
+blender = Blender(timeout=TIMEOUT)
 
-	def authenticate(self):
-		auth_header = None
 
-		if 'Authorization' in self.headers:
-			auth_header = bytes(self.headers.get('Authorization'), 'ASCII')
-
-#		print(self.headers)
-
-		if not auth_header or auth_header != srv_info['auth']:
-			self.do_AUTHHEAD()
-			self.wfile.write(b'Authorization required')
-			return False
-
-		# Valid request
-		self.do_HEAD()
-		return True
+class CustomServerHandler(http.server.BaseHTTPRequestHandler):
 
 	def do_HEAD(self):
 		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
+		self.send_header('Content-type', 'application/json')
 		self.end_headers()
 
 	def do_AUTHHEAD(self):
 		self.send_response(401)
-		self.send_header('WWW-Authenticate', 'Basic realm="Blender"')
-		self.send_header('Content-type', 'text/html')
+		self.send_header(
+			'WWW-Authenticate', 'Basic realm="Demo Realm"')
+		self.send_header('Content-type', 'application/json')
 		self.end_headers()
 
 	def do_GET(self):
-		if not self.authenticate():
-			return
+		key = self.server.get_auth_key()
 
-		self.wfile.write(b'1\n' if blender.state else b'0\n')
+		if not self.headers.get('Authorization'):
+			self.do_AUTHHEAD()
+			response = {
+				'success': False,
+				'error': 'No auth header received'
+			}
+		elif self.headers.get('Authorization') == 'Basic ' + str(key):
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			response = {
+				'success': True,
+				'message': 'Saftbandaren is online and lamp is {}'.format(blender.state)
+			}
+		else:
+			self.do_AUTHHEAD()
+			response = {
+				'success': False,
+				'error': 'Invalid credentials'
+			}
+
+		self.wfile.write(bytes(json.dumps(response), 'utf-8'))
 
 	def do_POST(self):
-		if not self.authenticate():
-			return
+		key = self.server.get_auth_key()
 
-		length = int(self.headers.get('content-length'))
-		data = self.rfile.read(length)
-		data = urllib.parse.parse_qs(data.decode('utf-8'))
+		''' Present frontpage with user authentication. '''
+		if self.headers.get('Authorization') is None:
+			self.do_AUTHHEAD()
+			response = {
+				'success': False,
+				'error': 'No auth header received'
+			}
 
-		if 'blender' in data:
-			state = data['blender'][0]
+		elif self.headers.get('Authorization') == 'Basic ' + str(key):
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
 
-			if state != '1' and state != '0':
-				return
+			response = {
+				'path': self.path,
+			}
 
-			state = True if state == '1' else False
+			# Add new paths here
+			# base_path = urlparse(self.path).path
+			# if base_path == '/path1':
+			#	# Do some work
+			#	pass
 
-			blender.set(state)
+			blender.toggle()
+			response['message'] = 'Saftblandaren toggled'
 
-		self.wfile.write(b'1\n' if blender.state else b'0\n')
+		else:
+			self.do_AUTHHEAD()
 
-blender = Blender(PIN_BLENDER, TIMEOUT_BLENDER_OFF)
+			response = {
+				'success': False,
+				'error': 'Invalid credentials'
+			}
 
-lp = srv_info['user'] + b':' + srv_info['pass']
-srv_info['auth'] = b'Basic ' + base64.b64encode(lp)
+		self.wfile.write(bytes(json.dumps(response), 'utf-8'))
 
-server = socketserver.TCPServer((srv_info['host'], 
-				 srv_info['port']), WebServer, False)
 
-server.allow_reuse_address = True # SO_REUSEADD (allows rebinding the port)
-server.server_bind()     # Manually bind, to support allow_reuse_address
-server.server_activate() # (see above comment)
+class CustomHTTPServer(http.server.HTTPServer):
+	key = ''
 
-handler = http.server.SimpleHTTPRequestHandler
+	def __init__(self, address, handlerClass=CustomServerHandler):
+		super().__init__(address, handlerClass)
 
-print("Server bound to interface '{:}', port {:}".format(srv_info['host'], 
-							 srv_info['port']))
-server.serve_forever()
+	def set_auth(self, username, password):
+		self.key = base64.b64encode(
+			bytes('%s:%s' % (username, password), 'utf-8')).decode('ascii')
+
+	def get_auth_key(self):
+		return self.key
+
+
+server = CustomHTTPServer(('', PORT))
+server.set_auth(USER, PASSWORD)
+try:
+	server.serve_forever()
+except Exception as e:
+	print(e)
+	pass
+blender.cleanup()
+server.server_close()
